752d3c2399fe8f9a7b36857f9d07e09a83560463
Florian Klampfer <mail@qwtel.com>
2021-04-16 17:50:21 +0700

    Commit

---
+++ src/perf_hooks.js
@@ -0,0 +1,27 @@
+const { 
+  performance,
+  Performance,
+  PerformanceEntry,
+  PerformanceMark,
+  PerformanceMeasure,
+  PerformanceNavigation,
+  PerformanceNavigationTiming,
+  PerformanceObserver,
+  PerformanceObserverEntryList,
+  PerformanceResourceTiming,
+  PerformanceTiming,
+} = globalThis;
+
+export {
+  performance,
+  Performance,
+  PerformanceEntry,
+  PerformanceMark,
+  PerformanceMeasure,
+  PerformanceNavigation,
+  PerformanceNavigationTiming,
+  PerformanceObserver,
+  PerformanceObserverEntryList,
+  PerformanceResourceTiming,
+  PerformanceTiming,
+};
